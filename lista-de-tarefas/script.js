668f1579e991b822a5db2a5b8b53e2dab6202834
Johann Eduardo <johanneduardo18@gmail.com>
2025-08-21 15:36:00 -0300

    Adicionando um local onde irao ficar alocados nossas tarefas (dentro de uma array)

--- lista-de-tarefas/script.js
+++ lista-de-tarefas/script.js
@@ -1,29 +1,39 @@
+let tarefas = [];
+
 function adicionarTarefa() {
- 
-  //puxamos do nosso html a tag input que vai ser utilizada
   const inputTarefa = document.getElementById("inputTarefa");
-  //puxando o valor digitado no input e aprensentando o valor
+
   const tarefa = inputTarefa.value.trim();
 
-  const mensagem = document.getElementById("mensagem")
+  const mensagem = document.getElementById("mensagem");
 
   if (tarefa === "") {
-    // se o valor do input or vazio, entao mostre uma mensagem de erro para o usuário
-    mensagem.style.color='red';
-    mensagem.textContent =
-      "Valor vazio, tente novamente";
+    mensagem.style.color = "red";
+
+    mensagem.textContent = "Valor vazio, tente novamente";
   } else {
-    mensagem.style.color='green';
+    mensagem.style.color = "green";
+
     mensagem.textContent = "Tarefa adicionada com sucesso!";
-    const listaTarefas = document.getElementById("listaTarefas");
-    //denominando que cada vez que o onclick for ativado, faça a criação de uma li que armazena um valor
+
+    tarefas.push(tarefa);
+
+    renderizarTarefas()
+  }
+  inputTarefa.value = "";
+}
+
+function renderizarTarefas() {
+  const listaTarefas = document.getElementById("listaTarefas");
+  listaTarefas.innerHTML = ""
+
+  let i = 0;
+
+  for (i; i < tarefas.length; i++) {
     let novaTarefa = document.createElement("li");
 
-    //li puxando o que o valor digitado no input para armazena-lo
-    novaTarefa.textContent = tarefa;
-    //denominando o caminho onde essa li tem de ser criada
+    novaTarefa.textContent = tarefas[i];
+
     listaTarefas.appendChild(novaTarefa);
   }
-  //puxando o valor
-  inputTarefa.value = "";
 }
